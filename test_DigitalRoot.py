96d7b2b7a2a1ea1cdae85d1bb80631f343543613
mici-que <Maria.Kohler@ibm.com>
2022-06-11 10:54:14 +0200

    1 Red

--- test_DigitalRoot.py
+++ test_DigitalRoot.py
@@ -1,4 +1,46 @@
 from DigitalRoot import main
+
+
 # initial test
 def test_init():
-    assert(main())==False
+    """no input given, return False"""
+    assert (main()) == False
+
+
+# 1 input validation
+def test_WrongInputType():
+    """input is string, return False"""
+    param = "1"
+    assert (main(param)) == False
+
+
+def test_WrongInputType2():
+    """input is list, return False"""
+    param = [
+        1,
+    ]
+    assert (main(param)) == False
+
+
+def test_NegativeNumber():
+    """input is negative, return False"""
+    param = -1
+    assert (main(param)) == False
+
+
+def test_ValidInput():
+    """input is non-negative integer, should return integer"""
+    param = 11
+    assert isinstance(main(param), int) == True
+
+
+def test_ValidInput2():
+    """input is non-negative integer, return value should have one digit"""
+    param = 11
+    assert len(str(main(param))) == 1
+
+
+def test_InputZero():
+    """input is zero, should return zero"""
+    param = 0
+    assert main(param) == 0
